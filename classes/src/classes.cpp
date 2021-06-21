adcc7ba491ee2accaed858ac41875a7e89b7f487
Stebin Yohannan <55764771+stebinyohannan@users.noreply.github.com>
2021-06-20 20:49:05 -0400

    Add files via upload

---
+++ classes/src/classes.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : classes.cpp
+// Author      : Stebin
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class Player
+{
+public:
+int x,y;
+int speed;
+};
+
+int main() {
+
+Player pla;
+pla.x = 1;
+pla.y = 2;
+pla.speed = 4;
+
+	return 0;
+}
